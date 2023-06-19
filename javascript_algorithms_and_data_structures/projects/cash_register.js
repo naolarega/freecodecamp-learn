6f109b07733250e60c20559e2128887202a1fcb2
naol arega <naol.dev@gmail.com>
2023-06-19 22:50:13 +0300

    fix: javascript cert project

--- javascript_algorithms_and_data_structures/projects/cash_register.js
+++ javascript_algorithms_and_data_structures/projects/cash_register.js
@@ -1,4 +1,4 @@
-const CURRENCY = {
+const CURRENCIES = {
     "PENNY":        0.01,
     "NICKEL":       0.05,
     "DIME":         0.1,
@@ -12,16 +12,49 @@ const CURRENCY = {
 
 /**
  * 
- * @param {number} price 
+ * Start from the biggest currency possible. dedact as much as possible
+ * then the next currency.
+ * 
+ * @param {number} price 0
  * @param {number} cash 
  * @param {[[string, number]]} cid 
  * @returns 
  */
 function checkCashRegister(price, cash, cid) {
-    const change = cash - price;
-    for(let cash of cid) {
-
+    let changeDue = parseFloat((cash - price).toFixed(2));
+    const change = [];
+    if(cid.reduce((prev, cur) => ["TOTAL", prev[1] += cur[1]])[1] == changeDue) {
+        return {
+            status: "CLOSED", change: cid
+        };
     }
-}
+    for(let currency of Object.keys(CURRENCIES).reverse()) {
+        const cashCurrencyInDrawer = cid.find(
+            cashInCurrency => (
+                cashInCurrency[0] == currency &&
+                cashInCurrency[1] >= CURRENCIES[currency]
+            )
+        )
+        if(
+            CURRENCIES[currency] <= changeDue &&
+            cashCurrencyInDrawer !== undefined
+        ) {
+            let currencyChange = Math.floor(changeDue / CURRENCIES[currency]) * CURRENCIES[currency];
+            if(changeDue - currencyChange >= 0) {
+                const currencyActualChange = currencyChange <= cashCurrencyInDrawer[1] ? currencyChange : cashCurrencyInDrawer[1];
+                change.push([currency, currencyActualChange]);
+                changeDue = parseFloat((changeDue - currencyActualChange).toFixed(2));
+            }
+        }
+    }
 
-checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+    if(changeDue !== 0) {
+        return {
+            status: "INSUFFICIENT_FUNDS", change: []
+        };
+    }
+
+    return {
+        status: "OPEN", change
+    };
+}
